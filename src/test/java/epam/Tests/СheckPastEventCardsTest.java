394258da90aa3e656c256d8b09782569a21c9499
echuyan <e.chuyan@gmail.com>
2021-08-25 20:23:03 +0300

    commit test

---
+++ src/test/java/epam/Tests/СheckPastEventCardsTest.java
@@ -0,0 +1,59 @@
+package epam.Tests;
+
+import epam.Pages.PastEventsPage;
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Assert;
+import org.junit.Test;
+
+import epam.Pages.EventsPage;
+import epam.Pages.MainPage;
+
+
+public class СheckPastEventCardsTest extends BaseTestClass {
+
+
+    /**
+     * Просмотр карточек мероприятий:
+     * 1 Пользователь переходит на вкладку events
+     * 2 Пользователь нажимает на Past Events
+     * 3 На странице отображаются карточки прошедших мероприятий.
+     * 4 В карточке указана информация о мероприятии:
+     * язык
+     * название мероприятия
+     * дата мероприятия
+     * информация о регистрации
+     * список спикеров // Минимально достаточное - проверить одну карточку. В идеале все что отображаются.
+     */
+    @Test
+    @DisplayName("Check past events cards")
+    public void checkEventCardTest() {
+
+        /**
+         *  создаем экземпляр главной страницы сайта
+         */
+
+        MainPage mainP = new MainPage(wd);
+        mainP.openEvents();
+
+        /**
+         *Создаем экземпляр страницы Events, проверяем ее открытие
+         */
+        EventsPage eventsP = new EventsPage(wd);
+        Assert.assertTrue(eventsP.isPageOpened());
+
+        /**
+         *Создаем экземпляр страницы Past Events, проверяем ее открытие
+         */
+        eventsP.openPastEvents();
+        PastEventsPage pastEvents = new PastEventsPage(wd);
+        Assert.assertTrue(pastEvents.isPageOpened());
+
+        /**
+         * Проверяем карточки прошедших мероприятий
+         */
+        pastEvents.checkCards();
+
+    }
+}
+
+
